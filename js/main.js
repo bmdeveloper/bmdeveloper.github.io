69663bf302805ca94af409b7f68b7c9cd4cc7b67
bmlegend <bmdeveloper@users.noreply.github.com>
2015-02-17 14:32:59 +0000

    added IIFE to remove variables from global scope

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-var photoFeedApp = angular.module('photoFeedApp', [
-  'ngRoute',
-  'photoFeedControllers',
-  'photoFeedServices'
-]);
+(function () {
+    'use strict';
+    var photoFeedApp = angular.module('photoFeedApp', [
+      'ngRoute',
+      'photoFeedControllers',
+      'photoFeedServices'
+    ]);
 
 
-//Configure routing of app
-photoFeedApp.config(['$routeProvider',
-     function ($routeProvider) {
-         $routeProvider.
-           when('/photos', {
-               templateUrl: 'views/photo-list.html',
-               controller: 'PhotoListController'
-           }).
-           when('/photos/:photoId', {
-               templateUrl: 'views/photo-details.html',
-               controller: 'PhotoDetailsController'
-           }).
-           otherwise({
-               redirectTo: '/photos'
-           });
-     }]);
-
-
+    //Configure routing of app
+    photoFeedApp.config(['$routeProvider',
+         function ($routeProvider) {
+             $routeProvider.
+               when('/photos', {
+                   templateUrl: 'views/photo-list.html',
+                   controller: 'PhotoListController'
+               }).
+               when('/photos/:photoId', {
+                   templateUrl: 'views/photo-details.html',
+                   controller: 'PhotoDetailsController'
+               }).
+               otherwise({
+                   redirectTo: '/photos'
+               });
+         }]);
+})();
--- js/services.js
+++ js/services.js
@@ -1,39 +1,42 @@
-﻿var photoFeedServices = angular.module('photoFeedServices', ['ngResource']);
+﻿(function () {
+    'use strict';
+    var photoFeedServices = angular.module('photoFeedServices', ['ngResource']);
 
-photoFeedServices.factory('flickrPhotosService', ['$resource', function ($resource) {
-    var flickrPhotoClass = {};
+    photoFeedServices.factory('flickrPhotosService', ['$resource', function ($resource) {
+        var flickrPhotoClass = {};
 
-    var publicPhotoBaseUrl = 'https://api.flickr.com/services/rest/';
+        var publicPhotoBaseUrl = 'https://api.flickr.com/services/rest/';
 
-    var api_key = '4e5708f9ab5d15dfb115bcd109c21743';
+        var api_key = '4e5708f9ab5d15dfb115bcd109c21743';
 
-    var photosData = $resource(publicPhotoBaseUrl, { method: 'flickr.photos.search', api_key: api_key, tag_mode: 'all', per_page: '20', extras: 'date_upload,owner_name', tags: 'sunset', format: 'json', nojsoncallback: '1' }, {
-        get: { method: 'GET',cache:true}
-    });
+        var photosData = $resource(publicPhotoBaseUrl, { method: 'flickr.photos.search', api_key: api_key, tag_mode: 'all', per_page: '20', extras: 'date_upload,owner_name', tags: 'sunset', format: 'json', nojsoncallback: '1' }, {
+            get: { method: 'GET', cache: true }
+        });
 
-    var photoDataById = $resource(publicPhotoBaseUrl, {
-        method: 'flickr.photos.getInfo', api_key: api_key, format: 'json', nojsoncallback: '1'
-    }, {
-        get: { method: 'GET', cache: true }
-    });
+        var photoDataById = $resource(publicPhotoBaseUrl, {
+            method: 'flickr.photos.getInfo', api_key: api_key, format: 'json', nojsoncallback: '1'
+        }, {
+            get: { method: 'GET', cache: true }
+        });
 
-    //variable to maintain a count of the next page load
-    flickrPhotoClass.pageToLoad = 2;
+        //variable to maintain a count of the next page load
+        flickrPhotoClass.pageToLoad = 2;
 
-    //function to get photo list
-    flickrPhotoClass.getPhotos = function (params, callback) { 
-        return photosData.get(params, callback);
-    }
+        //function to get photo list
+        flickrPhotoClass.getPhotos = function (params, callback) {
+            return photosData.get(params, callback);
+        };
 
-    //function to get photo information by id
-    flickrPhotoClass.getPhotoById = function (id, callback) {
-        return photoDataById.get({ photo_id: id }, callback);
-    }
+        //function to get photo information by id
+        flickrPhotoClass.getPhotoById = function (id, callback) {
+            return photoDataById.get({ photo_id: id }, callback);
+        };
 
-    return flickrPhotoClass;
+        return flickrPhotoClass;
 
 
-}]);
+    }]);
+})();
 
 
 
